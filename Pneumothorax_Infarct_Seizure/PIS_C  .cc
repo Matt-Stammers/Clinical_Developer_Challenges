52243e0950852e6f3f86e2f299994875976b64bd
Matt Stammers <matthewstammers@googlemail.com>
2017-06-23 00:18:40 +0100

    Update PIS_C  .cc

--- Pneumothorax_Infarct_Seizure/PIS_C  .cc
+++ Pneumothorax_Infarct_Seizure/PIS_C  .cc
@@ -1,5 +1,17 @@
-
+/* */
+New doctors working in a particular ED department are struggling to prioritise the management of 3 different common emergency problems if they coexist in the same patient at presentation. They are: 1) Massive Pneumothorax, 2) Ischemic shock and 3) Seizures.
+stay with me on this one.
+The rules are the following:
+- Massive pneumothorax trumps ischaemic shock in priority because it requires a chest drain to be inserted prior to anticoagulants. 
+- Uncontrolled Seizures trump pneumothorax as you need to control the seizures before the chest drain can be physically inserted. 
+- Ischemic shock trumps seizures because it is probably the cause of the fits and needs to be treated as a priority.
+The code should return both the priority to be treated and a single line treatment plan ;)
+You can write the code however you want in whatever language you want.
+Obviously this is a medical variation on rock, paper, scissors and the extremely new ED docs at this highly undersupported ED department will thank you for helping them with these highly difficult (and rare) possible clinical scenarios ;)
+/* */
+  
 // Example program by Jeremy Thornton - CDC
+  
 #include <iostream>
 #include <string>
 
