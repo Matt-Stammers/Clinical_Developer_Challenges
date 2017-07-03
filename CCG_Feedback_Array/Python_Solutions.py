25361ae1f62acc1d96dda7e7987bd15ea82ed5c5
Matt Stammers <matthewstammers@googlemail.com>
2017-07-03 22:15:39 +0100

    Update Python_Solutions.py

--- CCG_Feedback_Array/Python_Solutions.py
+++ CCG_Feedback_Array/Python_Solutions.py
@@ -2,6 +2,12 @@
 
 My rough and ready solution with minimal analysis - this will be fleshed out later
 
+Challenge: The CCG has a problem. They have done a survey of all the primary care patients in their region. The survey had a score from 1 to 10 asking patients to rate their care. All this data has been uploaded to a central computer system. Unfortunately, some silly folk have decided to put some silly numbers like -100 or 50 in the box. Can you write a simple function that will exclude all these outliers and sum up the totals that fit within the instructions of the survey?
+The CCG were going to ask PWC to sort this out for them at a cost of £20,000 but you have offered to do faster and better for consulting fees of just £4,000; Meanwhile offering to throw in a little analysis of your own 😉
+A sample of the CCG's array is given below:
+([1,5,7,81,1,4,7,8,-34,96,2,5,8,1,-124,753,9,5,4,3,13,7,90,5,1,4,8,3,6,7,7,8,500,-100,2,6,7,2,9,1,8,9,3,6])
+Your code function should come up with an answer to this array, and then the CCG will be happy to commission you.
+
 '''
 
 # This Function takes the CCG Test Array and returns the sum, count, valid count and mean ;)
