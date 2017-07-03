05b946b92717075f131e1d6e5cecaef8c916be1b
Matt Stammers <matthewstammers@googlemail.com>
2017-07-03 22:17:46 +0100

    Update Python_Versions.py

--- Bed_Day_Calculator/Python_Versions.py
+++ Bed_Day_Calculator/Python_Versions.py
@@ -1,3 +1,29 @@
+'''
+
+So a large district general hospital is trying to work predict how many bed days their patients will require, thus they can calculate capacity. The CQC have threatened to take them over if they don't improve.
+You are the only person in the hospital who can actually code (they use contractors to do any needed maintainence/dev) They have one piece of knowledge which they can use to make predictions and that is the following:
+- if a patient has stayed in hospital less than two days they are likely to stay only one more day (ie. n+1 days). 
+- if they have stayed 2-7 days they are likely to stay twice that time. 
+- if they have already been in hospital over a week they will normally stay 3 times that number of days on average.
+Given a snapshot of the hospital at the present time, can you build a function/algorithm that can be fed all this numeric data and return the number of bed days required?
+If you can solve it, you can be sure the hospital administrators will look favourably upon you when you submit that big grant proposal next month...
+
+Date: Tuesday 27th June 2017. 
+Day 1 patients: 240
+Day 2 patients: 143
+Day 3 patients: 490
+Day 4 patients: 136
+Day 5 patients: 59
+Day 6 patients: 90
+Day 7 patients: 20
+Day 8 patients: 137
+Day 13 patients: 70
+Day 15 patients: 36
+Day 25 patients: 26
+
+'''
+
+
 # My Version
 def bed_day_calculator(n, x):
 	"""Function will Calculate Bed Days."""
