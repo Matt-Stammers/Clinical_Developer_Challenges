5d10bab98dfb92c73ed2e236e81822e8fe100526
Matt Stammers <matthewstammers@googlemail.com>
2017-07-04 19:01:30 +0100

    Update JS_Solutions.js

--- CCG_Feedback_Array/JS_Solutions.js
+++ CCG_Feedback_Array/JS_Solutions.js
@@ -48,3 +48,11 @@ function antiScam(arr){
 }
 
 antiScam(votes);
+
+# David Anderson's version:
+
+console.log(
+  [1,5,7,81,1,4,7,8,-34,96,2,5,8,1,-124,753,9,5,4,3,13,7,90,5,1,4,8,3,6,7,7,8,500,-100,2,6,7,2,9,1,8,9,3,6].reduce((carry,vote) => {
+      return carry + (vote <= 10 && vote >= 1 ? vote : 0)
+    })
+);
